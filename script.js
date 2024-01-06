5a2d65cd1e6747734852dd5c664c70ad10f4f868
narvval <nicolastordini@gmail.com>
2024-01-06 16:29:26 -0300

    Fix extra round firing after win declared

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// begin with a function called getComputerChoice that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
+// Randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’ as the "computer's choice".
 function getComputerChoice() {
     random = Math.floor(Math.random() * 3);    
     switch (random) {
@@ -11,7 +11,7 @@ function getComputerChoice() {
     }
 }
 
-// Write a function that plays a single round of Rock Paper Scissors.
+// Play a single round of Rock Paper Scissors.
 function playRound(playerSelection, computerSelection) { 
     if (computerSelection === playerSelection) {
         return 'tie';
@@ -34,40 +34,58 @@ let player = document.querySelector('.player')
 
 // Add eventlisteners to buttons
 const buttons = document.querySelectorAll('button');
-
 buttons.forEach((button) => {
-
-    button.addEventListener('click', () => {
-        declareWinner();
-
-        let roundResult = playRound(button.className, getComputerChoice());
-        if (roundResult === 'tie') {
-            result.innerText = 'Tie';
-        }
-        else if (roundResult === 'win') {
-            result.innerText = 'Win';
-            player.innerText = playerScore;
-            playerScore++;
-        }
-        else {
-            result.innerText = 'Lose';
-            computer.innerText = computerScore;
-            computerScore++;
-        }
-
-    })
-
+    button.addEventListener('click', game);
 })
+
+// Play a round with the user's selection and display the results of the round
+function game(ev) {
+    let roundResult = playRound(ev.srcElement.className, getComputerChoice());
+    if (roundResult === 'tie') {
+        result.innerText = 'Tie';
+        console.log(`Tie. playerScore is ${playerScore}, computerScore is ${computerScore}`);
+
+    }
+    else if (roundResult === 'win') {
+        result.innerText = 'Win';
+        playerScore++;
+        player.innerText = playerScore;
+        console.log(`Player wins. playerScore is ${playerScore}, computerScore is ${computerScore}`);
+
+    }
+    else {
+        result.innerText = 'Lose';
+        computerScore++;
+        computer.innerText = computerScore;
+        console.log(`Player loses. playerScore is ${playerScore}, computerScore is ${computerScore}`);
+
+    }
+    declareWinner();
+
+}
 
 // Declare the winner and stop the game once one of the players reaches 5 pts
 function declareWinner() {
-    if (playerScore > 4) {
+    if (playerScore === 5) {
         // Display the results
         alert('You win!');
+
+        //Remove event listeners
+        buttons.forEach((button) => {
+            button.removeEventListener('click', game);
+        })
+
+        return '5';
     }
-    else if (computerScore > 4) {
+    else if (computerScore === 5) {
         // Display the results
         alert('You lose.');
+
+        //Remove event listeners
+        buttons.forEach((button) => {
+            button.removeEventListener('click', game);
+        })
+
+        return'5';
     }    
 }
-
